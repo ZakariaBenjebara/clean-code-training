d67a34654464df05c3c1bee8271eb58bef7cf8af
zakaria <zbenjebara@sqli.com>
2016-01-27 09:27:11 +0000

    Update

--- train-exercise/src/main/java/com/sqli/nespresso/train/wagon/Passenger.java
+++ train-exercise/src/main/java/com/sqli/nespresso/train/wagon/Passenger.java
@@ -2,8 +2,6 @@ package com.sqli.nespresso.train.wagon;
 
 public class Passenger extends Wagon {
 
-    private final String json = "{\"name\": \"zakaria\"}";
-
     public Passenger(WagonType wagonType) {
         super(wagonType);
     }
